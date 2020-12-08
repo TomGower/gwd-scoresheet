425d22f87e72aaa711eb836d5da73465b92bae62
TomGower <tom.gower@gmail.com>
2020-12-08 14:06:34 -0600

    Added score update functionality

--- src/functions/pickJoker.js
+++ src/functions/pickJoker.js
@@ -2,7 +2,15 @@ function pickJoker() {
   const radio = document.querySelectorAll('input[name=\'joker\']');
   for (let i = 0; i < radio.length; i += 1) {
     if (radio[i].checked) {
-      this.setState({ joker: radio[i].value });
+      const roundName = `r${i + 1}info`;
+      const jokerScore = this.state[roundName].score;
+      this.setState({
+        joker: radio[i].value,
+        jokerScore,
+        score: this.state.r1info.score + this.state.r2info.score + this.state.r3info.score
+        + this.state.r4info.score + this.state.r5info.score + this.state.r6info.score
+        + this.state.r7info.score + jokerScore,
+      });
       break;
     }
   }
