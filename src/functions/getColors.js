611f5db74dd751aaf508b90cc566f4d90ef0de35
TomGower <tom.gower@gmail.com>
2021-07-22 16:50:14 -0500

    Refactored to functional style

--- src/functions/getColors.js
+++ src/functions/getColors.js
@@ -1,15 +1,10 @@
 function getColors(scores) {
-  const colors = [];
-  for (let i = 0; i < scores.length; i += 1) {
-    if (parseInt(scores[i], 10) === 1) {
-      colors.push('green');
-    } else if (parseInt(scores[i], 10) === 0) {
-      colors.push('red');
-    } else {
-      colors.push('black');
-    }
-  }
-  return colors;
+  return scores.map((score) => {
+    const val = parseInt(score, 10);
+    if (val === 1) return 'green';
+    if (val === 0) return 'red';
+    return 'black';
+  });
 }
 
 export default getColors;
