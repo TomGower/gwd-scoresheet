0216961e54e1a189e2858d17ccd26e50aea9439a
TomGower <tom.gower@gmail.com>
2021-07-22 17:49:25 -0500

    Refactored to more functional style

--- src/functions/pickJoker.js
+++ src/functions/pickJoker.js
@@ -1,19 +1,14 @@
 function pickJoker() {
-  const radio = document.querySelectorAll('input[name=\'joker\']');
-  for (let i = 0; i < radio.length; i += 1) {
-    if (radio[i].checked) {
-      const roundName = `r${i + 1}info`;
-      const jokerScore = this.state[roundName].score;
-      this.setState({
-        joker: radio[i].value,
-        jokerScore,
-        score: this.state.r1info.score + this.state.r2info.score + this.state.r3info.score
-        + this.state.r4info.score + this.state.r5info.score + this.state.r6info.score
-        + this.state.r7info.score + jokerScore,
-      });
-      break;
-    }
-  }
+  const jokerRound = (Array.from(document.querySelectorAll('input[name=\'joker\']')).find((round) => round.checked));
+  const roundName = `${jokerRound.value}info`;
+  const jokerScore = this.state[roundName].score;
+  this.setState({
+    joker: jokerRound.value,
+    jokerScore,
+    score: this.state.r1info.score + this.state.r2info.score + this.state.r3info.score
+    + this.state.r4info.score + this.state.r5info.score + this.state.r6info.score
+    + this.state.r7info.score + jokerScore,
+  });
 }
 
 export default pickJoker;
