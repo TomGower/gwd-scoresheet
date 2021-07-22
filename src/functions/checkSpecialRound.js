055f339f23a036883ab94a47ac42c3922f52c4bb
TomGower <tom.gower@gmail.com>
2021-07-22 17:49:56 -0500

    Refactored to more functional style

--- src/functions/checkSpecialRound.js
+++ src/functions/checkSpecialRound.js
@@ -1,12 +1,28 @@
+// function checkSpecialRoundOriginal() {
+//   const specialRound = document.querySelectorAll('input[name=\'bonus\']');
+//   for (let i = 0; i < specialRound.length; i += 1) {
+//     let special = 0;
+//     if (i === 0) special = 1;
+//     else special = i + 2;
+//     const roundName = `r${special}info`;
+//     const currentInfo = this.state[roundName];
+//     if (specialRound[i].checked && !currentInfo.special) {
+//       currentInfo.special = true;
+//       currentInfo.scores = Array(16).fill(null);
+//       currentInfo.answers = Array(16).fill('');
+//       this.setState({
+//         [roundName]: currentInfo,
+//       });
+//     }
+//   }
+// }
+
 function checkSpecialRound() {
-  const specialRound = document.querySelectorAll('input[name=\'bonus\']');
-  for (let i = 0; i < specialRound.length; i += 1) {
-    let special = 0;
-    if (i === 0) special = 1;
-    else special = i + 2;
-    const roundName = `r${special}info`;
-    const currentInfo = this.state[roundName];
-    if (specialRound[i].checked && !currentInfo.special) {
+  const specialRound = (Array.from(document.querySelectorAll('input[name=\'bonus\']'))).filter((round) => round.checked);
+  specialRound.forEach((round) => {
+    const roundName = `${round.value}info`;
+    if (!this.state[roundName].special) {
+      const currentInfo = this.state[roundName];
       currentInfo.special = true;
       currentInfo.scores = Array(16).fill(null);
       currentInfo.answers = Array(16).fill('');
@@ -14,7 +30,7 @@ function checkSpecialRound() {
         [roundName]: currentInfo,
       });
     }
-  }
+  });
 }
 
 export default checkSpecialRound;
